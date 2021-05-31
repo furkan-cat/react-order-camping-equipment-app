5516ecf2c53e48847d76297a8a9d2ac194b15fd0
Furkan Çat <75113766+furkan-cat@users.noreply.github.com>
2021-05-31 19:03:19 +0300

    Add files via upload
    committed firebase

--- src/components/Products/AvailableProducts.js
+++ src/components/Products/AvailableProducts.js
@@ -1,40 +1,33 @@
+import { useEffect, useState } from "react";
+
 import Card from "../UI/Card";
 import ProductsItem from "./ProductsItem/ProductsItem";
 import classes from "./AvailableProducts.module.css";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Binocular",
-    description:
-      "Nikon's Aculon T02 binoculars are compact, lightweight and available in multiple colours.",
-    price: 149.49,
-  },
-  {
-    id: "m2",
-    name: "Coleman 6-Person Skydome",
-    description:
-      "Coleman’s Skydome series improves upon its classic dome tent by adding steeper walls to raise the headroom to six feet, in the case of the six-person model.",
-    price: 129.9,
-  },
-  {
-    id: "m3",
-    name: "Kelty Tuck 40",
-    description:
-      "The spacious shape allowed us to sleep in a variety of positions and still stay warm.",
-    price: 69.95,
-  },
-  {
-    id: "m4",
-    name: "Hults Bruk Tarnaby",
-    description:
-      "The 15-inch curved hickory shaft on the Tarnaby offers leverage for light-duty campground tasks, like making kindling and driving tent stakes into the ground.",
-    price: 99.11,
-  },
-];
-
 const AvailableProducts = () => {
-  const productList = DUMMY_MEALS.map((product) => (
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(
+        "https://react-http-fff0d-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+      const responseData = await response.json();
+      const loadedProducts = [];
+
+      for (const key in responseData) {
+        loadedProducts.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setProducts(loadedProducts);
+    };
+    fetchProducts();
+  }, []);
+  const productList = products.map((product) => (
     <ProductsItem
       id={product.id}
       key={product.id}
